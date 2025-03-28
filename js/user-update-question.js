2495ec213ceba02f5f745e2f0408f518ffcde679
CC <89727825+coDingcaRot@users.noreply.github.com>
2025-03-27 20:52:49 -0700

    Added update questions page, style. Modified Script

--- js/user-update-question.js
+++ js/user-update-question.js
@@ -1,34 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('updateQuestionForm');
+    document.getElementById('back-button').addEventListener('click', () => {
+        window.location.href = '/user-homepage.html'
+    });
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         
-        const id = document.getElementById("user-question-id").value;
-        const question = document.getElementById("user-question").value;
-        const answer = document.getElementById("user-answer").value;
+        const id = document.getElementById("questionId").value.trim();
+        const category = document.getElementById("category").value.trim();
+        const question = document.getElementById("question").value.trim();
+        const answer = document.getElementById("answer").value.trim();
 
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
+                const data = JSON.parse(this.responseText);
                 if (this.status == 200) {
-                    alert('Updated Question Successfully.');
+                    alert(`${data.message}`);
                 } else {
                     try {
-                        const data = JSON.parse(this.responseText);
-                        alert(data.message || 'Error updating question. Please try again');
+                        alert(`${data.message}`);
                     } catch (error) {
-                        alert('Error updating question. Please try again');
+                        alert('Internal Server Error');
                     }
                 }
             }
         };
 
-        xhttp.open('PUT', 'https://isa-project-backend-ultkx.ondigitalocean.app/putQuestion', true);
+        xhttp.open('PUT', 'https://isa-project-backend-ultkx.ondigitalocean.app/updateQuestion', true);
+        // xhttp.open('PUT', 'http://localhost:3000/updateQuestion', true);
         xhttp.setRequestHeader('Content-Type', 'application/json');
 
         const data = JSON.stringify({
             id,
+            category,
             question,
             answer
         });
