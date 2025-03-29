21933adacdbec55755f12a2a837e970bb863cfec
coDingcaRot <jonathanielalipes905@gmail.com>
2025-03-29 14:35:16 -0700

    Added get question page UI

--- js/user-get-question.js
+++ js/user-get-question.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('updateQuestionForm');
+    const form = document.getElementById('getQuestionForm');
+    document.getElementById('back-button').addEventListener('click', ()=>{
+        window.location.href = '/user-homepage.html';
+    });
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         
-        const id = document.getElementById("user-question-id").value;
+        const id = document.getElementById("questionId").value;
 
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
+                const data = JSON.parse(this.responseText);
                 if (this.status == 200) {
-                    alert('Updated Question Successfully.');
+                    alert(data.message);
                 } else {
                     try {
-                        const data = JSON.parse(this.responseText);
-                        alert(data.message || 'Error updating question. Please try again');
+                        alert(data.message || 'Error getting question. Please try again');
                     } catch (error) {
-                        alert('Error updating question. Please try again');
+                        alert('Error getting question. Please try again');
                     }
                 }
             }
         };
 
-        xhttp.open('GET', `https://isa-project-backend-ultkx.ondigitalocean.app/getQuestion/${id}`, true);
+        // xhttp.open('GET', `https://isa-project-backend-ultkx.ondigitalocean.app/getQuestion/${id}`, true);
+        xhttp.open('GET', `http://localhost:3000/getQuestion/${id}`, true);
+        xhttp.withCredentials = true; 
         xhttp.setRequestHeader('Content-Type', 'application/json');
         xhttp.send();
     });
