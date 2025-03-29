089def9337cc6cc43df4bd6793bc11a1734f1d68
CC <89727825+coDingcaRot@users.noreply.github.com>
2025-03-28 18:46:55 -0700

    Small Fixes and UI changes

--- js/check-authentication.js
+++ js/check-authentication.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
         if (this.readyState == 4) {
             // alert(`Authenticating`)
             const data = JSON.parse(this.responseText);
-            localStorage.setItem('welcomeState') == true;
 
             if (this.status == 200) {
-                alert(`Welcome ${data.username}`); 
+                if(window.location.pathname == '/user-homepage.html')
+                    alert(`Welcome ${data.username}`);
             } else {
-                window.location.href = 'unauthorized.html'
+                window.location.href = '/unauthorized.html'
             }
         }
     };
-    xhttp.open('GET', 'https://isa-project-backend-ultkx.ondigitalocean.app/authenticate', true);
-    // xhttp.open('GET', 'http://localhost:3000/authenticate', true);
+    // xhttp.open('GET', 'https://isa-project-backend-ultkx.ondigitalocean.app/authenticate', true);
+    xhttp.open('GET', 'http://localhost:3000/authenticate', true);
     xhttp.withCredentials = true; 
     xhttp.send();
 });
---
+++ js/unauthorized.js
@@ -0,0 +1,3 @@
+document.getElementById('go-back').addEventListener('click', () => {
+    window.location.href = '/login.html';
+});
